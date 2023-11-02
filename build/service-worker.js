a73d5a6eccbb694afe32eafd921b82ac2c5eaaf9
larbisamuel <larbisammy18@gmail.com>
2023-11-02 16:17:03 +0000

    comment out service worker

--- build/service-worker.js
+++ build/service-worker.js
@@ -3,14 +3,14 @@
 
 // Add this code to the top of your service worker source code
 
-self.__WB_MANIFEST = {
-    // List of all the assets that you want to cache
-    "assets": [
-      "index.html",
-      "main.css",
-      "bundle.js"
-    ]
-  };
+      // self.__WB_MANIFEST = {
+      //     // List of all the assets that you want to cache
+      //     "assets": [
+      //       "index.html",
+      //       "main.css",
+      //       "bundle.js"
+      //     ]
+      //   };
   
 // if ('serviceWorker' in navigator) {
 //     navigator.serviceWorker.register('/service-worker.js')
